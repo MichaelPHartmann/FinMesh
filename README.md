629f3d33b1258851c95a553111b60b665666c4ac
Joshua Hartmann <jchart@uvic.ca>
2019-03-04 02:34:02 -0800

    TBS: Added functions stock.py and outlined what needs to be done

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 #iexfinance-py
+This is a minimal wrapper for the IEX finance REST api: https://iexcloud.io/docs/api/
 
-This is a minimal wrapper for the iex finance REST api
+
+##Compatability with IEX cloud
+Differences between IEX cloud documentation and this API should be considered errors. Please raise an issue if you notice discrepencies.
--- stock.py
+++ stock.py
@@ -1,35 +1,160 @@
 import requests
 import os
 
-#{ticker} replaces the IEX-defined {symbol} in every case.
-#In all other instances naming is identical to the IEX API
-#Single quotes ('') only :)
+# API Endpoints for "stock":
+#   Balance Sheet
+#   Batch Requests
+#   Book
+#   Cash Flow
+#   Collections
+#   Company
+#   Delayed Quote
+#   Dividends
 
-#API Endpoints taken care of here:
-#Account usage
-#Chart
-#Financials
-#Income Statement
-#Balance Sheet
-#Cash Flow Statement
-#Key Stats
-#Price Target
-#Dividends
-#Relevant
-#Recommendation Trends
-#Analyst Estimates
-#News
+#   Earnings
+#   Earnings Today
+#   Effective Spread
+#   Estimates
+#   Financials
+#   Fund Ownership
+#   Historical Prices
+#   Income Statement
+#   Insider Transactions
+#   Institutional Ownership
+#   IPO Calendar
+#   Key Stats
+#   Largest Trades
+#   List
+#   Logo
+#   Market Volume (U.S.)
+#   News
+#   OHLC
+#   Open / Close Price
+#   Peers
+#   Previous Day Prices
+#   Price
+#   Price Target
+#   Quote
+#   Recommended Trends
+#   Relavent Stocks
+#   Sector Performance
+#   Splits
+#   Volume by Venue
 
-IEXCLOUD_STOCK_BASE_URL = 'https://cloud.iexapis.com/beta/stock/'
+IEX_STOCK_BASE_URL = 'https://cloud.iexapis.com/beta/stock/'
 
-def append_token(url):
+def _append_token(url):
     token = os.getenv('IEX_TOKEN')
-    return "{}?token={}".format(url, token)
+    return "{}&token={}".format(url, token)
 
-#Returns your account usage data, with option to return only specific datapoints
-IEX_STOCK_ACCOUNT_USAGE_URL = IEXCLOUD_STOCK_BASE_URL + 'account/usage/{type}'
-def account_usage(type="", format=None):
-    url = IEX_STOCK_ACCOUNT_USAGE_URL.replace('{type}', type)
+#   Balance Sheet
+IEX_STOCK_BALANCE_SHEET_URL = IEX_STOCK_BASE_URL + '{symbol}/balance-sheet?period={period}'
+def balance_sheet(symbol, period):
+    url = IEX_STOCK_BALANCE_SHEET_URL
+    url = url.replace('{symbol}', symbol)
+    url = url.replace('{period}', period)
+    result = requests.get(_append_token(url))
+    result = result.json()
+    return result
+
+#   Batch Requests
+def batch_requests():
+    raise ImplementationError("Function cannot be implemented.")
+
+#   Book
+IEX_STOCK_BOOK_URL = IEX_STOCK_BASE_URL + '{symbol}/book?'
+def book(symbol):
+    url = IEX_STOCK_BOOK_URL
+    url = url.replace('{symbol}', symbol)
+    result = requests.get(_append_token(url))
+    result = result.json()
+    return result
+
+#   Cash Flow
+IEX_STOCK_CASH_FLOW_URL = IEX_STOCK_BASE_URL + '{symbol}/cash_flow?'
+def cash_flow(symbol, period=None):
+    url = IEX_STOCK_CASH_FLOW_URL
+    url = url.replace('{symbol}', symbol)
+    if period:
+        url = url + "period={}".format(period)
+    result = requests.get(_append_token(url))
+    result = result.json()
+    return result
+
+#   Collections
+IEX_STOCK_COLLECTION_URL = IEX_STOCK_BASE_URL + 'market/collection/{collectionType}?collectionName={collectionName}'
+def collection(collectionType, collectionName):
+    url = IEX_STOCK_COLLECTION_URL
+    url = url.replace('{collectionType}', collectionType)
+    url = url.replace('{collectionName}', collectionName)
+    result = requests.get(_append_token(url))
+    result = result.json()
+    return result
+
+#   Company
+IEX_STOCK_COMPANY_URL = IEX_STOCK_BASE_URL + '{symbol}/company'
+def company(symbol):
+    url = IEX_STOCK_COMPANY_URL
+    url = url.replace('{symbol}', symbol)
+    result = requests.get(_append_token(url))
+    result = result.json()
+    return result
+
+#   Delayed Quote
+IEX_STOCK_DELAYED_QUOTE_URL = IEX_STOCK_BASE_URL + '{symbol}/delayed-quote'
+def delayed_quote(symbol):
+    url = IEX_STOCK_DELAYED_QUOTE_URL
+    url = url.replace('{symbol}', symbol)
+    result = requests.get(_append_token(url))
+    result = result.json()
+    return result
+
+#   Dividends
+IEX_DIVIDENDS_QUOTE_URL = IEX_STOCK_BASE_URL + 'stock/{symbol}/dividends/{range}'
+def dividends(symbol, range):
+    url = IEX_DIVIDENDS_QUOTE_URL
+    url = url.replace('{symbol}', symbol)
+    url = url.replace('{range}', range)
+    result = requests.get(_append_token(url))
+    result = result.json()
+    return result
+
+#   Earnings
+#   Earnings Today
+#   Effective Spread
+#   Estimates
+#   Financials
+#   Fund Ownership
+#   Historical Prices
+#   Income Statement
+#   Insider Transactions
+#   Institutional Ownership
+#   IPO Calendar
+#   Key Stats
+#   Largest Trades
+#   List
+#   Logo
+#   Market Volume (U.S.)
+#   News
+#   OHLC
+#   Open / Close Price
+#   Peers
+#   Previous Day Prices
+#   Price
+#   Price Target
+#   Quote
+#   Recommended Trends
+#   Relavent Stocks
+#   Sector Performance
+#   Splits
+#   Volume by Venue
+
+
+
+# Returns historical stock prices for specified company
+IEX_STOCK_CHART_URL = IEXCLOUD_STOCK_BASE_URL + '{symbol}/chart/{range}/{date}'
+def chart(symbol, range, date='', format=None):
+    url = IEX_STOCK_CHART_URL.replace('{symbol}', symbol).replace('{range}', range).replace('{date}', date)
     result = requests.get(append_token(url))
     result = result.json()
 
@@ -43,10 +168,10 @@ def account_usage(type="", format=None):
 
     return result
 
-#Returns historical stock prices for specified company
-IEX_STOCK_CHART_URL = IEXCLOUD_STOCK_BASE_URL + '{ticker}/chart/{range}/{date}'
-def chart(ticker, range, date='', format=None):
-    url = IEX_STOCK_CHART_URL.replace('{ticker}', ticker).replace('{range}', range).replace('{date}', date)
+# Returns financials overview (limited data) for specified company
+IEX_STOCK_FINANCIALS_URL = IEXCLOUD_STOCK_BASE_URL + '{symbol}/financials?period={period}'
+def financials(symbol, period, format=None):
+    url = IEX_STOCK_FINANCIALS_URL.replace('{symbol}', symbol).replace('{period}', period)
     result = requests.get(append_token(url))
     result = result.json()
 
@@ -60,10 +185,10 @@ def chart(ticker, range, date='', format=None):
 
     return result
 
-#Returns financials overview (limited data) for specified company
-IEX_STOCK_FINANCIALS_URL = IEXCLOUD_STOCK_BASE_URL + '{ticker}/financials?period={period}'
-def financials(ticker, period, format=None):
-    url = IEX_STOCK_FINANCIALS_URL.replace('{ticker}', ticker).replace('{period}', period)
+# Returns income statement for specified company
+IEX_STOCK_INCOME_STATEMENT_URL = IEXCLOUD_STOCK_BASE_URL + 'stock/{symbol}/income?period={period}'
+def income_statement(symbol, period, format=None):
+    url = IEX_STOCK_INCOME_STATEMENT_URL.replace('{symbol}', symbol).replace('{period}', period)
     result = requests.get(append_token(url))
     result = result.json()
 
@@ -77,10 +202,10 @@ def financials(ticker, period, format=None):
 
     return result
 
-#Returns income statement for specified company
-IEX_STOCK_INCOME_STATEMENT_URL = IEXCLOUD_STOCK_BASE_URL + 'stock/{ticker}/income?period={period}'
-def income_statement(ticker, period, format=None):
-    url = IEX_STOCK_INCOME_STATEMENT_URL.replace('{ticker}', ticker).replace('{period}', period)
+# Returns balance sheet for specified company
+IEX_STOCK_BALANCE_SHEET_URL = IEXCLOUD_STOCK_BASE_URL + 'stock/{symbol}/balance-sheet?period={period}'
+def balance_sheet(symbol, period, format=None):
+    url = IEX_STOCK_BALANCE_SHEET_URL.replace('{symbol}', symbol).replace('{period}', period)
     result = requests.get(append_token(url))
     result = result.json()
 
@@ -94,10 +219,10 @@ def income_statement(ticker, period, format=None):
 
     return result
 
-#Returns balance sheet for specified company
-IEX_STOCK_BALANCE_SHEET_URL = IEXCLOUD_STOCK_BASE_URL + 'stock/{ticker}/balance-sheet?period={period}'
-def balance_sheet(ticker, period, format=None):
-    url = IEX_STOCK_BALANCE_SHEET_URL.replace('{ticker}', ticker).replace('{period}', period)
+# Returns cash flow statement for specified company
+IEX_STOCK_CASH_FLOW_URL = IEXCLOUD_STOCK_BASE_URL + 'stock/{symbol}/cash-flow?period={period}'
+def cash_flow(symbol, period, format=None):
+    url = IEX_STOCK_CASH_FLOW_URL.replace('{symbol}', symbol).replace('{period}', period)
     result = requests.get(append_token(url))
     result = result.json()
 
@@ -111,10 +236,10 @@ def balance_sheet(ticker, period, format=None):
 
     return result
 
-#Returns cash flow statement for specified company
-IEX_STOCK_CASH_FLOW_URL = IEXCLOUD_STOCK_BASE_URL + 'stock/{ticker}/cash-flow?period={period}'
-def cash_flow(ticker, period, format=None):
-    url = IEX_STOCK_CASH_FLOW_URL.replace('{ticker}', ticker).replace('{period}', period)
+# Returns key statistics for specified company
+IEX_STOCK_KEY_STAT_URL = IEXCLOUD_STOCK_BASE_URL + 'stock/{symbol}/stats/{stat}'
+def key_stat(symbol, stat, format=None):
+    url = IEX_STOCK_KEY_STAT_URL.replace('{symbol}', symbol).replace('{stat}', stat)
     result = requests.get(append_token(url))
     result = result.json()
 
@@ -128,10 +253,10 @@ def cash_flow(ticker, period, format=None):
 
     return result
 
-#Returns key statistics for specified company
-IEX_STOCK_KEY_STAT_URL = IEXCLOUD_STOCK_BASE_URL + 'stock/{ticker}/stats/{stat}'
-def key_stat(ticker, stat, format=None):
-    url = IEX_STOCK_KEY_STAT_URL.replace('{ticker}', ticker).replace('{stat}', stat)
+# Returns the analyst price target for specified company
+IEX_STOCK_PRICE_TARGET_URL = IEXCLOUD_STOCK_BASE_URL + 'stock/{symbol}/price-target'
+def price_target(symbol, format=None):
+    url = IEX_STOCK_PRICE_TARGET_URL.replace('{symbol}', symbol)
     result = requests.get(append_token(url))
     result = result.json()
 
@@ -145,10 +270,10 @@ def key_stat(ticker, stat, format=None):
 
     return result
 
-#Returns the analyst price target for specified company
-IEX_STOCK_PRICE_TARGET_URL = IEXCLOUD_STOCK_BASE_URL + 'stock/{ticker}/price-target'
-def price_target(ticker, format=None):
-    url = IEX_STOCK_PRICE_TARGET_URL.replace('{ticker}', ticker)
+# Returns dividend information for specified company
+IEX_STOCK_DIVIDENDS_URL = IEXCLOUD_STOCK_BASE_URL + 'stock/{symbol}/dividends/{range}'
+def dividends(symbol, range, format=None):
+    url = IEX_STOCK_DIVIDENDS_URL.replace('{symbol}', symbol).replace('{range}', range)
     result = requests.get(append_token(url))
     result = result.json()
 
@@ -162,10 +287,10 @@ def price_target(ticker, format=None):
 
     return result
 
-#Returns dividend information for specified company
-IEX_STOCK_DIVIDENDS_URL = IEXCLOUD_STOCK_BASE_URL + 'stock/{ticker}/dividends/{range}'
-def dividends(ticker, range, format=None):
-    url = IEX_STOCK_DIVIDENDS_URL.replace('{ticker}', ticker).replace('{range}', range)
+# Returns relevant stocks, similar to peers (true/false specifies whether it is the peer list)
+IEX_STOCK_RELEVANT_URL = IEXCLOUD_STOCK_BASE_URL + 'stock/{symbol}/relevant'
+def relevant(symbol, format=None):
+    url = IEX_STOCK_RELEVANT_URL.replace('{symbol}')
     result = requests.get(append_token(url))
     result = result.json()
 
@@ -179,10 +304,10 @@ def dividends(ticker, range, format=None):
 
     return result
 
-#Returns relevant stocks, similar to peers (true/false specifies whether it is the peer list)
-IEX_STOCK_RELEVANT_URL = IEXCLOUD_STOCK_BASE_URL + 'stock/{ticker}/relevant'
-def relevant(ticker, format=None):
-    url = IEX_STOCK_RELEVANT_URL.replace('{ticker}')
+# Returns IB analyst recommendations
+IEX_STOCK_RECOMENDATION_URL = IEXCLOUD_STOCK_BASE_URL + 'stock/{symbol}/recommendation-trends'
+def recommendation_trends(symbol, format=None):
+    url = IEX_STOCK_RECOMENDATION_URL.replace('{symbol}', symbol)
     result = requests.get(append_token(url))
     result = result.json()
 
@@ -196,10 +321,10 @@ def relevant(ticker, format=None):
 
     return result
 
-#Returns IB analyst recommendations
-IEX_STOCK_RECOMENDATION_URL = IEXCLOUD_STOCK_BASE_URL + 'stock/{ticker}/recommendation-trends'
-def recommendation_trends(ticker, format=None):
-    url = IEX_STOCK_RECOMENDATION_URL.replace('{ticker}', ticker)
+# Returns earnings estimates for the specified company
+IEX_STOCK_ESTIMATES_URL = IEXCLOUD_STOCK_BASE_URL + 'stock/{symbol}/estimates'
+def analyst_estimates(symbol, format=None):
+    url = IEX_STOCK_ESTIMATES_URL.replace('{symbol}', symbol)
     result = requests.get(append_token(url))
     result = result.json()
 
@@ -213,27 +338,10 @@ def recommendation_trends(ticker, format=None):
 
     return result
 
-#Returns earnings estimates for the specified company
-IEX_STOCK_ESTIMATES_URL = IEXCLOUD_STOCK_BASE_URL + 'stock/{ticker}/estimates'
-def analyst_estimates(ticker, format=None):
-    url = IEX_STOCK_ESTIMATES_URL.replace('{ticker}', ticker)
-    result = requests.get(append_token(url))
-    result = result.json()
-
-    if format == 'pandas':
-        # Do your pandas formatting here
-        pass
-
-    if format == 'numpy':
-        #do your numpy formatting here
-        pass
-
-    return result
-
-#Returns 10 most recent news items for specified company
-IEX_STOCK_NEWS_URL = IEXCLOUD_STOCK_BASE_URL + 'stock/{ticker}/news'
-def news(ticker, format=None):
-    url = IEX_STOCK_NEWS_URL.replace('{ticker}', ticker)
+# Returns 10 most recent news items for specified company
+IEX_STOCK_NEWS_URL = IEXCLOUD_STOCK_BASE_URL + 'stock/{symbol}/news'
+def news(symbol, format=None):
+    url = IEX_STOCK_NEWS_URL.replace('{symbol}', symbol)
     result = requests.get(append_token(url))
     result = result.json()
 
