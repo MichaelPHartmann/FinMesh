769847bbd0e428252f2848ecc115a99e6b25949c
Joshua Hartmann <jchart@uvic.ca>
2019-03-06 14:37:56 -0800

    Added account endpoint to iex and abstracted common functions

---
+++ iex/_common.py
@@ -0,0 +1,27 @@
+import requests
+import os
+
+VERBOSE = False # Turn on explanitory output
+
+def vprint(*args, **kwargs):
+    if VERBOSE:
+        print(*args, **kwargs)
+
+def append_token(url):
+    token = os.getenv('IEX_TOKEN')
+    return f"{url}&token={token}"
+
+def get_iex_json_request(url):
+    url = append_token(url)
+    vprint(f"Making request: {url}")
+    result = requests.get(url)
+    vprint(f"Request status code: {result.status_code}")
+    if result.status_code != 200:
+        raise BaseException(result.text)
+    result = result.json()
+    return result
+
+def replace_url_var(url, **kwargs):
+    for key, value in kwargs.items():
+        url = url.replace('{' + key + '}', value)
+    return url
---
+++ iex_test.py
@@ -0,0 +1,7 @@
+import iex.account
+import iex.stock
+
+if __name__ == "__main__":
+    import json
+    print("Running developer tests...")
+    
