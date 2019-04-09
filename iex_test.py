e224afc1fd95874038ba83e67b504bb59b7f9a9b
MichaelPHartman <Michael@MacBook-Pro-5.local>
2019-04-08 23:11:34 -0700

    yield curve collection

--- iex_test.py
+++ iex_test.py
@@ -1,9 +1,13 @@
 import iex.account
 import iex.stock
 import usgov.yieldcurve
+import dict
 
 if __name__ == "__main__":
     import json
     print("Running developer tests...")
 
-print(raw_curve())
+raw = usgov.yieldcurve.raw_yield_curve()
+print(raw.keys())
+
+print()
--- usgov/yieldcurve.py
+++ usgov/yieldcurve.py
@@ -1,10 +1,42 @@
 import requests
+import xmltodict
 import os
 
-GOV_YIELD_URL = 'https://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData?$filter=month%28NEW_DATE%29%20eq%205%20and%20year%28NEW_DATE%29%20eq%202013'
+GOV_YIELD_URL = 'https://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData?$filter=month(NEW_DATE)%20eq%204%20and%20year(NEW_DATE)%20eq%202019'
 
-#General get for raw json data in dict form
-def raw_curve():
-    with open(GOV_YIELD_CURVE) as curve:
-        curve = xmltodict.parse(curve.read())
-    return curve
+#Formatting of XML to Python Dict
+curve = requests.get(GOV_YIELD_URL)
+curve = xmltodict.parse(curve.content)
+
+#Definitions for various datapoints
+#This will be based around retrieving the n last dates or average of n days.
+feed = curve['feed']
+entry = feed['entry']
+content = entry[0]['content']
+content = content['m:properties']
+one_month_yield = content['d:BC_1MONTH']['#text']
+two_month_yield = content['d:BC_2MONTH']['#text']
+three_month_yield = content['d:BC_3MONTH']['#text']
+six_month_yield = content['d:BC_6MONTH']['#text']
+one_year_yield = content['d:BC_1YEAR']['#text']
+two_year_yield = content['d:BC_2YEAR']['#text']
+three_year_yield = content['d:BC_3YEAR']['#text']
+five_year_yield = content['d:BC_5YEAR']['#text']
+ten_year_yield = content['d:BC_10YEAR']['#text']
+twenty_year_yield = content['d:BC_20YEAR']['#text']
+thirty_year_yield = content['d:BC_30YEAR']['#text']
+yield_curve_values = {
+        '1 Month' : one_month_yield,
+        '2 Month' : two_month_yield,
+        '3 Month' : three_month_yield,
+        '6 Month' : six_month_yield,
+        '1 Year' : one_year_yield,
+        '2 Year' : two_year_yield,
+        '3 Year' : three_year_yield,
+        '5 Year' : five_year_yield,
+        '10 Year' : ten_year_yield,
+        '20 Year' : twenty_year_yield,
+        '30 Year' : thirty_year_yield,
+    }
+
+print(yield_curve_values)
