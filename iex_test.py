1a9eb8fe420de7b7403d01884de3e87df0a9de2d
MichaelPHartman <Michael@MacBook-Pro-5.local>
2019-04-09 14:54:37 -0700

    finish

--- iex_test.py
+++ iex_test.py
@@ -1,13 +1,11 @@
 import iex.account
 import iex.stock
 import usgov.yieldcurve
+from usgov.yieldcurve import *
 import dict
 
 if __name__ == "__main__":
     import json
     print("Running developer tests...")
 
-raw = usgov.yieldcurve.raw_yield_curve()
-print(raw.keys())
-
-print()
+print(one_month_yield)
