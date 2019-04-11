d587f02d92d09db1977a5051b9dc8f3b782a0904
MichaelPHartman <Michael@MacBook-Pro-5.local>
2019-04-11 15:23:25 -0700

    file type endpoints

--- usgov/series.py
+++ usgov/series.py
@@ -1,4 +1,5 @@
 import os
+import csv
 import json
 import requests
 import xmltodict
@@ -18,6 +19,12 @@ def fred_series(series, file_type):
         result = result.json()
     elif file_type == 'xml':
         result = xmltodict.parse(result.content)
+    elif file_type == 'txt':
+        csv_file = f'Series_Data/{series}'
+        open(csv_file, 'w')
+        result = csv.write(result)
+    #elif file_type == 'xls':
+        #use pandas to write to excel?
     return result
 
 print(fred_series('GNPCA', 'json'))
