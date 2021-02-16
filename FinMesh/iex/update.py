dd9164a0faa51c584cfc5471a84515ebf1c59e0e
MichaelPHartmann <michaelpeterhartmann94@gmail.com>
2021-02-15 17:08:29 -0800

    Started with new endpoints

--- FinMesh/iex/update.py
+++ FinMesh/iex/update.py
@@ -1,22 +1,22 @@
 from iex import stock
 
-ticker = 'FARM'
-
 # TO-DO #
-# Advanced Stats
-# Charts
-# Extended Hours Quote
-# Financials as Reported
-# Intraday Prices
-# Open/Close Prices
-# Real-Time Quote
-# SEC Filings
-# Technical Indicators
+#   Advanced Stats
+IEX_ADVANCED_STATS_URL = prepend_iex_url('stock') + '{symbol}/advanced-stats'
+def advanced_stats(symbol, vprint=False):
+    pass
 
+#   Financials as Reported
+IEX_FINANCIALS_REPORTED_URL = prepend_iex_url('time-series') + 'reported_financials/{symbol}/{filing}'
+def financials_as_reported(symbol, filing, vprint=False):
+    pass
 
+#   Intraday Prices
+IEX_INTRADAY_URL = prepend_iex_url('stock') + '{symbol}/intraday-prices'
+def intraday_prices(symbol, vprint=False):
+    pass
 
-## Test of possibly deprecated endpoints ##
-
-
-
-print(stock.sector_performance())
+#   Technical Indicators
+IEX_TECHNICAL_URL = prepend_iex_url('stock') + '{symbol}/indicator/{indicator-name}'
+def technical_indicators(symbol, indicator, vprint=False):
+    pass
