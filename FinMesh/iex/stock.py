e8031f7d034929dff4b7646bf022876b94732f27
MichaelPHartmann <michaelpeterhartmann94@gmail.com>
2021-02-20 18:22:07 -0800

    Finished updating iex stock endpoint

--- FinMesh/iex/stock.py
+++ FinMesh/iex/stock.py
@@ -1,4 +1,4 @@
-from ._common import *
+from _common import *
 
 #   Advanced Stats
 IEX_ADVANCED_STATS_URL = prepend_iex_url('stock') + '{symbol}/advanced-stats'
@@ -70,6 +70,17 @@ def financials(symbol, period=None, vprint=False):
     url += f'period={period}' if period else ''
     return get_iex_json_request(url, vprint=vprint)
 financials.__doc__='Returns a brief overview of a company\'s financial statements.'
+
+#   Financials as Reported
+IEX_FINANCIALS_REPORTED_URL = prepend_iex_url('time-series') + 'reported_financials/{symbol}/{filing}?'
+def financials_as_reported(symbol, filing, specification=None, vprint=False):
+    # Returns 10-K or 10-Q filings exactly as reported to the SEC
+    url = replace_url_var(IEX_FINANCIALS_REPORTED_URL, symbol=symbol, filing=filing)
+    if specification:
+        url =+ specification
+    ## Needs support for specific queries of date-specific or n-last filings
+    return get_iex_json_request(url, vprint=vprint)
+financials_as_reported.__doc__='Returns \'10-K\' or \'10-Q\' filings exactly \as reported to the SEC. Queries based on \'last\', \'on\', \or \'from\' specifications must be entered \as directed by IEX documentation.'
 
 #   Fund Ownership
 IEX_FUND_OWNERSHIP_URL = prepend_iex_url('stock') + '{symbol}/fund-ownership?'
