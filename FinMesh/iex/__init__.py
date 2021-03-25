303fdd0028282b886031935b6660be03dd51288f
MichaelPHartmann <michaelpeterhartmann94@gmail.com>
2021-03-24 21:03:26 -0700

    Add class attribute creator

--- FinMesh/iex/__init__.py
+++ FinMesh/iex/__init__.py
@@ -8,13 +8,20 @@ class IEXStock:
 
     def balance_sheet(self):
         result = stock.balance_sheet(self.ticker, self.period, self.last)
+        self.balance_sheet = result
         return result
-
 
     def income_statement(self):
         result = stock.income_statement(self.ticker, self.period, self.last)
+        self.income_statement = result
         return result
 
     def cash_flow_statement(self):
         result = stock.cash_flow(self.ticker, self.period, self.last)
+        self.cash_flow_statement = result
+        return result
+
+    def price(self):
+        result = stock.price(self.ticker)
+        self.price = result
         return result
