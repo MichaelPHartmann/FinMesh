f0ed3c85c0fedb623680c672d5ac3c01095f3e48
MichaelPHartmann <mph101mph@gmail.com>
2021-05-14 19:51:49 -0700

    Start environment variable setup script. Add bash_builder and set_all into namespace.

--- FinMesh/auxiliary/token_setup.py
+++ FinMesh/auxiliary/token_setup.py
@@ -10,3 +10,22 @@ First, there is very little chance of someone accidentally pushing secrets becau
 Second, global environment variables are accessible ANYWHERE, meaning you can have multiple projects in separate directories and you only have to set up your tokens once.
 Third, it is a generally accepted good practice for handling sensitive API token data.
 """
+
+# environment setup class
+# types of environment variables that can be set as class attributes
+# method for ingesting desired variables to be modified or added
+# python bash method - subprocess.Popen(bash commands as strings) first import
+#
+class FinMeshSetup():
+    def __init__(self):
+        self.variables_to_define = ['IEX_TOKEN', 'IEX_SANDBOX_TOKEN', 'IEX_SANDBOX', 'FRED_TOKEN']
+
+    def _bash_builder(variable, value):
+        """Builds a bash command to export the specified variable"""
+        command = f'export {variable}={value}'
+        return command
+
+    def set_all_environment_variables():
+        """Prompts the user to enter all of the needed variables for FinMesh.
+        Useful for first-time users who have not set any variables yet.
+        If you would like to skip through a particular variable, just hit enter to assign a blank; no variable will be created in your environment."""
