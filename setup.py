4706cb7a97dc03feb900836e34062d609aaff897
MichaelPHartmann <mph101mph@gmail.com>
2021-06-06 22:31:58 -0700

    Version-2.0a3_Pre_Release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name = "FinMesh",
-    version = "2.0a2",
+    version = "2.0a3",
     author = "Michael and Josh Hartmann",
     author_email = "mph101mph@gmail.com",
     description = "A Python wrapper to bring together various financial APIs.",
