16e66f66d9ecc5a5dbeaf0d66235016fe24c6e5d
MichaelPHartmann <mph101mph@gmail.com>
2021-09-18 14:52:03 +0200

    Add Sphinx support

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,11 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'FinMesh'
+copyright = '2021, Michael Hartmann'
+author = 'Michael Hartmann'
 
-release = '0.1'
-version = '0.1.0'
+version = '1.9.0'
 
 # -- General configuration
 
