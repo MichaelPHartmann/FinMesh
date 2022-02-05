72d9e97a57eac0a02094431d9461c2742ac90346
MichaelPHartmann <mph101mph@gmail.com>
2022-02-05 14:56:32 +0200

    Start refactoring _common module into a class

--- FinMesh/iex/_common.py
+++ FinMesh/iex/_common.py
@@ -9,6 +9,7 @@ def arg_to_bool(string):
     else:
         return False
 
+# This may not be needed
 def prepend_iex_url(section, external=False):
     sandboxState = arg_to_bool(os.getenv('SANDBOX'))
     if sandboxState is True:
@@ -45,3 +46,78 @@ def replace_url_var(url, **kwargs):
     for key, value in kwargs.items():
         url = url.replace('{' + key + '}', value)
     return url
+
+class iexCommon():
+    """
+    A class that wraps all the common methods used in the IEX requests.
+    A class is not / was not strictly neccesary at the beginning of this project.
+    Adding support for external tokens meant that code was being repeatedly passed as arguments between functions.
+    A class means that these attributes can be shared easily, and it offers some streamlining solutions.
+    Certain functions can be done away with completely and others can be broken up more.
+    It also means that creating new functions for new endpoints is much easier and could be implemented by an end user.
+
+    :param endpoint: The endpoint you want to access. Specifically this must be the exact string used in the url that points to the desired endpoint.
+    :type endpoint: String, required
+    :param symbol: The symbol for the stock you would like to request data for.
+    :type symbol: String, required
+    :param external: Whether you want to use environment variables or not for the token and sandbox settings.
+    :type external: Boolean or String. Default is False, insert token here to bypass using environment variables.
+    """
+
+    def __init__(self, endpoint, symbol, external=False):
+        self.url = f'iexapis.com/stable/{section}/{symbol}/{endpoint}?'
+        if external not False:
+            self.token = external
+            self.parse_token_sandbox(self.token)
+        else:
+            self.sandbox_state = self.get_env_sandbox()
+            self.token = self.get_env_token()
+        if sandbox_state is True:
+            self.url = 'https://sandbox.' + self.url
+        else:
+            self.url = 'https://cloud.' + self.url
+
+    # Simple string comprehension and conversion for use with environment variable settings
+    def arg_to_bool(self, string):
+        affirmative = ['True','TRUE','true','Yes','YES','yes','On','ON','on']
+        if string in affirmative:
+            return True
+        else:
+            return False
+
+    # Takes a token string and sets the sandbox state accordingly.
+    # All IEX tokens use a 'T' as the first char for sandbox tokens
+    def parse_token_sandbox(self, token):
+        if self.token[0] == 'T':
+            setattr(self, 'sandbox_state', True)
+        else:
+            setattr(self, 'sandbox_state', False)
+
+    # Gets the sandbox state from environment variable string and parses it into a boolean
+    def get_env_sandbox(self):
+        sandbox_state = self.arg_to_bool(os.getenv('SANDBOX'))
+        return sandbox_state
+
+    # Gets the IEX token from environment variables based on whether sandbox state is enabled.
+    def get_env_token(self):
+        if sandboxState:
+            token = os.getenv('IEX_SANDBOX_TOKEN')
+        else:
+            token = os.getenv('IEX_TOKEN')
+        return token
+
+    # Adds query paramters to the url
+    def add_query_params_to_url(self, **query_params):
+        for key, value in queries.items():
+            self.url += (f"&{key}={value}")
+        return self.url
+
+
+
+
+# Basic layout of function in pretty much psuedo code
+def test_iex_retrieval(symbol, **query_params):
+    """DOCUMENTATION"""
+    common = iexCommon(url, symbol, external).execute()
+
+    common.get_request
